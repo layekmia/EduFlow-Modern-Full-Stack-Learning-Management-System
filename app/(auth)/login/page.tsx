c68ba83dcd99b1deadd39ad38ee46c9f57cc141c
Layek Miah <layekofficial63@gmail.com>
2026-02-17 15:13:36 +0600

    successfully implemented email authentication with otp verification

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -11,14 +11,19 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { authClient } from "@/lib/auth-client";
-import { Github, Loader2 } from "lucide-react";
-import { useTransition } from "react";
+import { Github, Loader2, Send } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
 import { toast } from "sonner";
 
 export default function LoginForm() {
   const [githubPending, startGithubTransition] = useTransition();
+  const [emailPending, startEmailPending] = useTransition();
 
-  async function signInWithGithub() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+
+  function signInWithGithub() {
     startGithubTransition(async () => {
       await authClient.signIn.social({
         provider: "github",
@@ -35,11 +40,35 @@ export default function LoginForm() {
     });
   }
 
+  function signInWithEmail() {
+    if (!email || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      return toast.error("Invalid email address");
+    }
+
+    startEmailPending(async () => {
+      await authClient.emailOtp.sendVerificationOtp({
+        email: email, // required
+        type: "sign-in",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Email sent");
+            router.push(`/verify-request?email=${email}`);
+          },
+          onError: () => {
+            toast.error("Error sending email");
+          },
+        },
+      });
+    });
+  }
+
   return (
     <Card>
       <CardHeader>
         <CardTitle className="text-xl">Welcome back!</CardTitle>
-        <CardDescription>Login with your Github Email Account</CardDescription>
+        <CardDescription>
+          Login with your Github or Email Account
+        </CardDescription>
       </CardHeader>
 
       <CardContent className="flex flex-col gap-4">
@@ -71,9 +100,26 @@ export default function LoginForm() {
         <div className="grid gap-3">
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
-            <Input type="email" placeholder="m@example.com" />
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
           </div>
-          <Button>Continue with Email</Button>
+          <Button onClick={signInWithEmail} disabled={emailPending}>
+            {emailPending ? (
+              <>
+                <Loader2 className="size-4 animate-spin" />{" "}
+                <span>Loading...</span>
+              </>
+            ) : (
+              <>
+                <Send className="size-4" /> <span>Continue with Email</span>
+              </>
+            )}
+          </Button>
         </div>
       </CardContent>
     </Card>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,39 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+"use client";
 
-export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-  if (!session) {
-    return <div>Not authenticated</div>;
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+export default function Home() {
+  const { data: session, isPending, error, refetch } = authClient.useSession();
+
+  const router = useRouter();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/login"); // redirect to login page
+          toast.success("Signout successfully");
+        },
+      },
+    });
   }
+
+  if (isPending) return <Loader2 className="size-10 animate-spin" />;
   return (
     <div>
-      <h1>Welcome {session.user.name}</h1>
+      {session ? (
+        <>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Logout</Button>
+        </>
+      ) : (
+        <Link href={"/login"}>Login</Link>
+      )}
     </div>
   );
 }
