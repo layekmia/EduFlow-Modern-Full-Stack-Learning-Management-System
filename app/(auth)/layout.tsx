af2368a69f739d8ff927829a7e29cca42972c869
Layek Miah <layekofficial63@gmail.com>
2026-02-17 08:29:36 +0600

    setup shadcn ui, dark mode theme , sperate layout via route groups for auth and design login page

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,46 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/logo.png";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex min-h-svh flex-col items-center justify-center">
+      <Link
+        href="/"
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute top-4 left-4",
+        })}
+      >
+        <ArrowLeft className="size-4" />
+        Back
+      </Link>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <Image src={logo} width={40} height={40} alt="Logo" />
+          EduFlow
+        </Link>
+        {children}
+        <div className="text-balance text-center text-xs text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <span className="hover:text-primary hover:underline">
+            Terms of service
+          </span>{" "}
+          and policy{" "}
+          <span className="hover:text-primary hover:underline">
+            Privacy policy
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
