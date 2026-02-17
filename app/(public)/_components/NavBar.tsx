90c35bbd875b78e449352d5a0ffa7b638e8a386c
Layek Miah <layekofficial63@gmail.com>
2026-02-17 19:56:53 +0600

    used custom theme from tweakcn

--- app/(public)/_components/NavBar.tsx
+++ app/(public)/_components/NavBar.tsx
@@ -62,7 +62,7 @@ export default function NavBar() {
               <>
                 <Link
                   href={"/login"}
-                  className={buttonVariants({ variant: "secondary" })}
+                  className={buttonVariants({ variant: "outline" })}
                 >
                   Login
                 </Link>
