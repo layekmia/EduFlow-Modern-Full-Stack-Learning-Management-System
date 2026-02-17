4a86809fd8a843f810ca6922fa049fb31be48a7a
Layek Miah <layekofficial63@gmail.com>
2026-02-17 17:30:57 +0600

    created hero section , cards sections and navigation

---
+++ app/(public)/_components/NavBar.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/logo.png";
+import { ThemeToggle } from "@/components/ui/ThemeToggle";
+import { authClient } from "@/lib/auth-client";
+import { buttonVariants } from "@/components/ui/button";
+
+interface NavItem {
+  name: string;
+  href: string;
+}
+
+const navItems: NavItem[] = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Courses",
+    href: "/courses",
+  },
+  { name: "Dashboard", href: "/dashboard" },
+];
+
+export default function NavBar() {
+  const { data: session, isPending } = authClient.useSession();
+
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur-[backdrop-filter]:bg-background">
+      <div className="container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
+        <Link href="/" className="flex items-center space-x-2 mr-4">
+          <Image src={logo} alt="Logo" className="size-9" />
+          <span className="font-bold">EduFlow</span>
+        </Link>
+
+        {/* Desktop navigation */}
+        <nav className="hidden md:flex md:flex-1 md:items-center md:justify-between">
+          <div className="flex items-center space-x-2">
+            {navItems.map((item) => (
+              <Link
+                className="text-sm font-medium transition-colors hover:text-primary"
+                key={item.name}
+                href={item.href}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <ThemeToggle />
+            {isPending ? null : session ? (
+              <>Logged IN</>
+            ) : (
+              <>
+                <Link
+                  href={"/login"}
+                  className={buttonVariants({ variant: "secondary" })}
+                >
+                  Login
+                </Link>
+                <Link
+                  href={"/login"}
+                  className={buttonVariants()}
+                >
+                  Get Started
+                </Link>
+              </>
+            )}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
---
+++ app/(public)/layout.tsx
@@ -0,0 +1,14 @@
+import NavBar from "./_components/NavBar";
+
+export default function LayoutPublic({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+     <NavBar/>
+      <main className="container mx-auto px-4 md:px-6 lg:px-8">{children}</main>
+    </div>
+  );
+}
