a0bb419da9ea9f3ebc3c34cbb8aac453910fb35f
Layek Miah <layekofficial63@gmail.com>
2026-02-18 12:01:23 +0600

    setup react hook form and zod for validation

--- app/admin/courses/create/page.tsx
+++ app/admin/courses/create/page.tsx
@@ -1,5 +1,56 @@
+import { buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { courseSchema, courseSchemaType } from "@/lib/zodSchemas";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 export default function CourseCreationPage() {
+  const form = useForm<courseSchemaType>({
+    resolver: zodResolver(courseSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      fileKey: "",
+      price: 0,
+      duration: 0,
+      level: "Beginner",
+      category: "",
+      status: "Draft",
+      slug: "",
+      smallDescription: "",
+    },
+  });
+
   return (
-    <div>page</div>
-  )
+    <>
+      <div className="flex items-center gap-4">
+        <Link
+          href="/admin/courses"
+          className={buttonVariants({ variant: "outline", size: "icon" })}
+        >
+          <ArrowLeft className="size-4" />
+        </Link>
+
+        <h1 className="text-2xl font-bold">Create Courses</h1>
+      </div>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Basic Information</CardTitle>
+          <CardDescription>
+            Provide basic information about the course
+          </CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+    </>
+  );
 }
---
+++ lib/zodSchemas.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+const courseLevels = ["Beginner", "Intermediate", "Advanced"] as const;
+const courseStatus = ["Draft", "Published", "Archived"] as const;
+
+export const courseSchema = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Title must be at least 3 characters long" })
+    .max(100, { message: "Title must be at most 100 characters long" }),
+
+  description: z
+    .string()
+    .min(20, { message: "Description must be at least 20 characters long" }),
+
+  fileKey: z.string().min(1, { message: "File is required" }),
+
+  price: z.coerce.number().min(1, { message: "Price must be at least 1" }),
+  duration: z.coerce.number().min(1, { message: "Duration must be at least 1 hour" }),
+
+  level: z.enum(courseLevels, { message: "level is required" }),
+
+  category: z.string(),
+
+  smallDescription: z
+    .string()
+    .min(3, { message: "Small description must be at least 3 characters" })
+    .max(200, { message: "Small description must be at most 200 characters" }),
+
+  slug: z
+    .string()
+    .min(3, { message: "Slug must be at least 3 characters long" }),
+
+  status: z.enum(courseStatus, { message: "Status is required" }),
+});
+
+export type courseSchemaType = z.infer<typeof courseSchema>;
