e3767349d86c85b3712b044a1dec6a8ca8583263
Layek Miah <layekofficial63@gmail.com>
2026-03-03 20:52:27 +0600

    complte user dashbaord index page

---
+++ app/data/user/get-enrolled-courses.ts
@@ -0,0 +1,33 @@
+import prisma from "@/lib/prisma";
+import { requireUser } from "./require-user";
+
+
+export async function getEnrolledCourses() {
+    const user = await requireUser();
+
+    const data = await prisma.enrollment.findMany({
+        where: { userId: user.id, status: 'Active' }, select: {
+            course: {
+                select: {
+                    id: true,
+                    smallDescription: true,
+                    title: true,
+                    fileKey: true,
+                    level: true,
+                    slug: true,
+                    duration: true,
+                    chapter: {
+                        select: {
+                            id: true,
+                            lessons: {
+                                select: { id: true }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    return data;
+}
